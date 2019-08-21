0d0956035eb8ecdeea730624ccab4b88b07b7f42
Jack Mattar <jackeline_mattar@hotmail.com>
2019-08-21 13:43:06 -0300

    função cifrar e decifrar, preciso ainda limpar os códigos e fazer a interação com os outros arquivos

--- src/cipher.js
+++ src/cipher.js
@@ -1,63 +1,130 @@
-window.cipher = {
-  encode: cipherencode,
-  decode: cipherdecode
-};
+/*window.cipher = {
+  encode: cipherencode(),
+  decode: cipherdecode()
+};*/
 
-function cipherencode(){
-
-  event.preventDefault()
-
-  // RECEBE O VALOR DE "DESLOCAMENTO"
-  let displacement= document.getElementById("displacement").value
-
-  // TRANSFORMA VALOR DE "DESLOCAMENTO" EM NÚMERO
-  let dispnum= Number(displacement)
-
-  // RECEBE O VALOR DE "CIFRAR"
-  let txtcode= document.getElementById("textencoded").value
-  
-  // DESCOBRE ASCII DE "CIFRAR" E TRANSFORMA EM NÚMERO
-  let asciinum= Number(txtcode.charCodeAt(0))
-  
-  // RECEBE LUGAR ONDE EXIBIRÁ RESULTADO
-  let res= document.getElementById("result")
-
- //CIFRA
-  let decodedtxt= ( (asciinum - 65) + dispnum ) % 26 + 65
-
-  //DESCOBRE A STRING DO NÚMERO CIFRADO
-  let newcode= String.fromCharCode(decodedtxt)
-
-  res.innerHTML= (`Seu texto: ${newcode}`)
-
-}
-
-
-function cipherdecode(){
+function encode(){
 
   event.preventDefault()
 
-  // RECEBE O VALOR DE "DESLOCAMENTO"
-  let displacement= document.getElementById("displacement").value
 
-  // TRANSFORMA VALOR DE "DESLOCAMENTO" EM NÚMERO
-  let dispnum= Number(displacement)
+  let displacement= document.getElementById("offset").value
 
-  // RECEBE O VALOR DE "DECIFRAR"
-  let txtcode= document.getElementById("textdecoded").value
-  
-  // DESCOBRE ASCII DE "DECIFRAR" E TRANSFORMA EM NÚMERO
-  let asciinum= Number(txtcode.charCodeAt(0))
-  
-  // RECEBE LUGAR ONDE EXIBIRÁ RESULTADO
+  let disptonum= Number(displacement)
+
   let res= document.getElementById("result")
 
-  //DESCIFRA
-  let decodedtxt= ( (asciinum - 65) - dispnum ) % 26 + 65
+  let txtencode= new Array;
+  
+  let textcipher= new Array;
 
-  //DESCOBRE A STRING DO NÚMERO DECIFRADO
-  let newcode= String.fromCharCode(decodedtxt)
 
-  res.innerHTML= (`Seu texto: ${newcode}`)
 
+  function taketxt(){
+
+  var txtcipher= document.getElementById("stringencode").value
+   return txtcipher.toUpperCase();
+  }
+
+  function uppertxt(txt){
+    return txt.toUpperCase();
+  }
+
+  for (i in taketxt()){
+    
+    let n1=  (taketxt().charCodeAt(i))
+   
+
+    if (n1 >= 65 && n1 <= 90){
+
+      let n2 = txtencode.push(n1)
+
+      let decodedtxt= ((txtencode[i]) - 65 + disptonum ) % 26 + 65
+      //console.log(decodedtxt)
+
+      let novamensagem= String.fromCharCode(decodedtxt)
+      //console.log(novamensagem)
+
+      textcipher.push (novamensagem)
+      // console.log(textcipher)
+    
+
+    } else {
+
+      txtencode.push(taketxt().charAt(i))
+      textcipher.push(taketxt().charAt(i))
+      console.log(textcipher)
+
+    }
+
+      textcipher.join
+  }
+  
+ return res.innerHTML=(textcipher.join(""))
 }
+
+function decode(){
+
+ // event.preventDefault()
+
+
+  let displacement= document.getElementById("displacement").value
+
+  let disptonum= Number(displacement)
+
+  let res= document.getElementById("result")
+
+  let txtencode= new Array;
+  
+  let textcipher= new Array;
+
+
+
+  function taketxt(){
+
+    var txtcipher= document.getElementById("stringdecoded").value
+   return(uppertxt(txtcipher));
+  }
+
+  function uppertxt(txt){
+    return txt.toUpperCase();
+  }
+
+  for (i in taketxt()){
+    
+    let n1=  (taketxt().charCodeAt(i))
+   
+
+    if (n1 >= 65 && n1 <= 90){
+
+      let n2 = txtencode.push(n1)
+      console.log(n2)
+
+      let decodedtxt= ((txtencode[i]) - 65 + disptonum ) % 26 + 65
+      //console.log(decodedtxt)
+
+      let novamensagem= String.fromCharCode(decodedtxt)
+      //console.log(novamensagem)
+
+      textcipher.push (novamensagem)
+      // console.log(textcipher)
+    
+
+    } else {
+
+      txtencode.push(taketxt().charAt(i))
+      textcipher.push(taketxt().charAt(i))
+      console.log(textcipher)
+
+    }
+
+      textcipher.join
+  }
+  
+ return res.innerHTML=(textcipher.join(""))
+}
+
+
+
+
+
